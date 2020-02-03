6f5716c084b4ff02d8a42272b4f631165a472340
foyez <foyezar@yahoo.com>
2020-02-03 21:33:38 +0600

    add .env file

--- src/apis/axios-posts.js
+++ src/apis/axios-posts.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'https://jsonplaceholder.typicode.com'
+	baseURL: process.env.REACT_APP_POSTS_URL // https://jsonplaceholder.typicode.com
 });
 
 export default instance;
